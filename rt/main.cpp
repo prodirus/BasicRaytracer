2113e552fc17cd015ca89a88e7324c0fb63c6873
Prodirus <prodirus@gmail.com>
2023-09-29 13:48:59 +0100

    redo collision testing code

--- rt/main.cpp
+++ rt/main.cpp
@@ -3,13 +3,34 @@
 #include "ray.h"
 
 #include <iostream>
+#include <array>
+
+double hit_sphere(const point3& center, double radius, const ray& r) {
+    vec3 oc = r.origin() - center;
+    auto a = r.direction().length_squared();
+    auto half_b = dot(oc, r.direction());
+    auto c = oc.length_squared() - radius * radius;
+    auto discriminant = half_b * half_b - a * c;
+
+    if (discriminant < 0) {
+        return -1.0;
+    }
+    else {
+        return (-half_b - sqrt(discriminant)) / a;
+    }
+}
 
 color ray_color(const ray& r) {
+    auto t = hit_sphere(point3(0, 0, -1), 0.5, r);
+    if (t > 0.0) {
+        vec3 N = unit_vector(r.at(t) - vec3(0, 0, -1));
+        return 0.5 * color(N.x() + 1, N.y() + 1, N.z() + 1);
+    }
+
     vec3 unit_direction = unit_vector(r.direction());
     auto a = 0.5 * (unit_direction.y() + 1.0);
     return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
 }
-
 
 int main() {
 
@@ -59,4 +80,8 @@ int main() {
     }
 
     std::clog << "\rDone.                 \n";
+
+    std::cin.clear(); // reset any error flags
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // ignore any characters in the input buffer until we find an enter character
+    std::cin.get(); // get one more char from the user
 }
